7926a13dd0d1266e96cd7d22091f112ce279fe78
unknown <USER@WSPL-0137.wolkensoftware.com>
2021-10-06 18:05:34 +0530

    Initial Commit

--- Functions/Func.java
+++ Functions/Func.java
@@ -1,6 +1,14 @@
 public class Func{
 	public static void main(String[] args){
 		FuncCall s = new FuncCall();
+		System.out.println("=======================================================");
 		s.display();
+		System.out.println("=======================================================");
+		s.display("This is Java");
+		System.out.println("=======================================================");
+		s.display("Java was developed by James Gosling in ",1995);
+		System.out.println("=======================================================");
+		s.display("Testing",1,2,3,4,5);
+		System.out.println("=======================================================");
 	}
 }
--- Functions/FuncCall.java
+++ Functions/FuncCall.java
@@ -2,4 +2,16 @@ public class FuncCall{
 	public void display(){
 		System.out.println("Hello World");
 	}
+	public void display(String word){
+		System.out.println(word);
+	}
+	public void display(String word, int number){
+		System.out.println(word+" "+number);
+	}
+	public void display(String word,int ...numbers){
+		System.out.println(word);
+		for(int i: numbers){
+			System.out.println(i);
+		}
+	}
 }
